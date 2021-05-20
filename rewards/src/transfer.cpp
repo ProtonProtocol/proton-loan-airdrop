971e37091944aaa3a56787c18311f5b09c71af14
Metallicus Dev <>
2021-05-20 14:09:59 -0600

    remove duplicate check

--- rewards/src/transfer.cpp
+++ rewards/src/transfer.cpp
@@ -3,20 +3,18 @@
 namespace proton {
 void rewards::on_transfer(const name& from, const name& to,
                           const asset& quantity, const string& memo) {
-  check(from == get_self() || to == get_self(), "not involved in transfer");
-
   // Skip if outgoing
   if (from == get_self()) {
     return;
   }
 
+  // Validate transfer
+  check(to == get_self(), "invalid to account");
+
   // Skip if deposit from system accounts
   if (from == "eosio.stake"_n || from == "eosio.ram"_n || from == "eosio"_n) {
     return;
   }
-
-  // Validate transfer
-  check(to == get_self(), "invalid to account");
 
   // Deposit
   name token_contract = get_first_receiver();
